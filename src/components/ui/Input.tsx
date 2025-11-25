603731ab42ff7b57cc36346cf81e00242aff75ab
romanshrestha20 <stha.roman20@gmail.com>
2025-11-25 23:25:20 +0200

    style: Improve Input component layout and styling for consistency

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -7,18 +7,27 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const Input = ({ label, error, ...props }: InputProps) => {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col w-full gap-1">
       {label && (
-        <label className="text-sm font-medium text-gray-700">{label}</label>
+        <label className="text-sm font-medium text-foreground">
+          {label}
+        </label>
       )}
+
       <input
-        value={props.value}
         {...props}
-        className={` text-gray-700 border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500
-        ${error ? "border-red-500" : "border-gray-300"}
-      `}
+        className={`
+          w-full px-3 py-2 rounded-lg
+          bg-input text-foreground
+          border border-border
+          placeholder:text-muted-foreground
+          focus:outline-none focus:ring-2 focus:ring-primary/40
+          transition
+          ${error ? "border-error focus:ring-error/40" : ""}
+        `}
       />
-      {error && <p className="text-sm text-red-500">{error}</p>}
+
+      {error && <p className="text-sm text-error">{error}</p>}
     </div>
   );
 };
