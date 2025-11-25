413d126d326776db424e4cb96033e839b1791555
romanshrestha20 <stha.roman20@gmail.com>
2025-11-25 21:46:33 +0200

    refactor: Update import path for NavbarLink component to improve module structure

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,6 +2,6 @@ import { UserMenu } from './UserMenu';
 export { CartSideBar } from './CartSideBar';
 export { Navbar } from './Navbar';
 export { UserMenu } from './UserMenu';
-export { NavbarLink } from './NavbarLink';
+export { NavbarLink } from '../ui/NavbarLink';
 export { NavbarSearch } from './NavbarSearch';
 export { MobileMenu } from './MobileMenu';
